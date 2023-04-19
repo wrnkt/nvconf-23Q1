c0a97b4be6586234c084e9a35856c489ded58675
Niko <wrnkt1@gmail.com>
2023-04-19 07:25:24 -0400

    Update config.

--- lua/config1/init.lua
+++ lua/config1/init.lua
@@ -1,2 +1,3 @@
 require("config1.remap")
 require("config1.set")
+require("config1.packer")
--- lua/config1/remap.lua
+++ lua/config1/remap.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("n", "<leader>h", vim.cmd.noh)
+
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -22,3 +24,4 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
